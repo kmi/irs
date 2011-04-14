afec9c9d5ede3625a67daebb4561932362141cf0
Dave Lambert <d.j.lambert@gmail.com>
2011-04-14 17:43:47 +0100

    tests: Run NIH app tests if account details are found

--- tests/top.lisp
+++ tests/top.lisp
@@ -2,6 +2,16 @@
 
 (in-package #:irs.tests)
 
+(defvar *suites* '(api-rest-suite
+		   #+:irs-use-lispweb
+                   browser-interface-suite
+		   #+:irs-lispworks
+                   cs-invocation
+                   grounding-suite
+		   #+:irs-lispworks
+                   monitoring-suite
+                   trusted-travel-suite))
+
 (defun setup ()
   (pushnew "irs:tests;ontologies;" ocml:*ontology-path*)
   (webonto:require-ontologies '(:dino :irs-tests))
@@ -13,18 +23,19 @@
   #+:irs-lispworks
   (webonto:require-ontologies '(:international-system-units
                                 :execution-history))
-  (webonto:setup-library))
+  (webonto:setup-library)
+
+  ;; If account details for the NIH services are available, run the
+  ;; tests.
+  (when (probe-file (concatenate 'string cl-user::*irs-home*
+				 "/apps/nih/ontologies/nih-application/secrets.lisp"))
+    (irs:use-application :nih)
+    (require :nih.tests)
+    (setf *suites* (append *suites*
+			   (list (intern "NIH-SUITE" :IRS.APPLICATIONS.NIH.TESTS))))))
 
 (defun run-all-tests ()
   (format t "~%")
-  (dolist (suite '(api-rest-suite
-		   #+:irs-use-lispweb
-                   browser-interface-suite
-		   #+:irs-lispworks
-                   cs-invocation
-                   grounding-suite
-		   #+:irs-lispworks
-                   monitoring-suite
-                   trusted-travel-suite))
+  (dolist (suite *suites*)
     (format t "Running ~A: " (symbol-name suite))
     (run! suite)))
